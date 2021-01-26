fa823f8d5775a44b72762ca2dee2af059480db12
Viktor Todorov <vikotodorov02@gmail.com>
2021-01-26 12:40:48 +0200

    fix decode

--- Server/decode.js
+++ Server/decode.js
@@ -12,62 +12,62 @@ function Decode(fPort, bytes, variables) {
     var frequency;
     var amplitude;
     var seconds;
-    var message = String.fromCharCode.apply(null, bytes);
+    var message = atob(bytes);
     message = message.split(/(?=[thpgfas])/g);
   
-	for (i of message) {
-	  switch(i[0]) {
-		  case 't':
-			  var str = i.slice(1);
-			  temp = parseFloat(str);
-			  break;
-		  case 'h':
-			  var str = i.slice(1);
-			  humidity = parseFloat(str);
-			  break;
-		  case 'p':
-			  var str = i.slice(1);
-			  pressure = parseFloat(str);
-			  break;
-		  case 'g':
-			  var str = i.slice(1);
-			  gas = parseFloat(str);
-			  break;
-		  case 'f':
-			  var str = i.slice(1);
-			  frequency = parseFloat(str);
-			  break;
-		  case 'a':
-			  var str = i.slice(1);
-			  amplitude = parseFloat(str);
-			  break;
-		  case 's':
-			  var str = i.slice(1);
-			  seconds = parseFloat(str);
-			  break;
-	  }	
-	}
+    for (i of message) {
+        switch(i[0]) {
+            case 't':
+                var str = i.slice(1);
+                temp = parseFloat(str);
+                break;
+            case 'h':
+                var str = i.slice(1);
+                humidity = parseFloat(str);
+                break;
+            case 'p':
+                var str = i.slice(1);
+                pressure = parseFloat(str);
+                break;
+            case 'g':
+                var str = i.slice(1);
+                gas = parseFloat(str);
+                break;
+            case 'f':
+                var str = i.slice(1);
+                frequency = parseFloat(str);
+                break;
+            case 'a':
+                var str = i.slice(1);
+                amplitude = parseFloat(str);
+                break;
+            case 's':
+                var str = i.slice(1);
+                seconds = parseFloat(str);
+                break;
+          }
+    }   
 
-	if (!isNaN(temp)) {
-	  	jsonObject.temp = temp;
-	}
-	if (!isNaN(humidity)) {
-	  	jsonObject.humidity = humidity;
-	}
-	if (!isNaN(pressure)) {
-	  	jsonObject.pressure = pressure;
-	}
-	if (!isNaN(gas)) {
-	  	jsonObject.gas = gas;
-	}
-	if (!isNaN(frequency)) {
-	  	jsonObject.frequency = frequency;
-	}
-	if (!isNaN(amplitude)) {
-	  	jsonObject.amplitude = amplitude;
-	}
-	if (!isNaN(seconds)) {
-	  	jsonObject.seconds = seconds;
-	}
-	return JSON.stringify(jsonObject);
+    if (!isNaN(temp)) {
+        jsonObject.temp = temp;
+    }
+    if (!isNaN(humidity)) {
+        jsonObject.humidity = humidity;
+    }
+    if (!isNaN(pressure)) {
+        jsonObject.pressure = pressure;
+    }
+    if (!isNaN(gas)) {
+        jsonObject.gas = gas;
+    }
+    if (!isNaN(frequency)) {
+        jsonObject.frequency = frequency;
+    }
+    if (!isNaN(amplitude)) {
+        jsonObject.amplitude = amplitude;
+    }
+    if (!isNaN(seconds)) {
+        jsonObject.seconds = seconds;
+    }
+    return jsonObject;
 }
