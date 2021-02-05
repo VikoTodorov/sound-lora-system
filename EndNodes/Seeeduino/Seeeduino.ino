394817532c4795e367d904d59a9da1ce99dc8f40
Viktor Todorov <vikotodorov02@gmail.com>
2021-02-06 01:04:01 +0200

    fixed adc sampling, amplitude, fourier still need fix

--- EndNodes/Seeeduino/Seeeduino.ino
+++ EndNodes/Seeeduino/Seeeduino.ino
@@ -5,16 +5,16 @@
 #include "BME680_SETUP.hh"
 #include "mic.hh"
 
-#define sRate 12500                  //sample rate of ADC
+#define sRate 12500                   //sample rate of ADC
 
-const uint16_t dSize = 1024;         //used to set number of samples
+const uint16_t dSize = 1024;          //used to set number of samples
 const uint16_t dHalfSize = 512;
-const byte gClk = 3;                 //used to define which generic clock we will use for ADC
-const byte intPri = 0;               //used to set interrupt priority for ADC
-const uint8_t cDiv = 1;              //divide factor for generic clock
+const byte gClk = 3;                  //used to define which generic clock we will use for ADC
+const byte intPri = 0;                //used to set interrupt priority for ADC
+const uint8_t cDiv = 1;               //divide factor for generic clock
 
-volatile int16_t aDCVal[dSize];      //array to hold ADC samples
-volatile uint16_t countSamples = 0;  //tracks how many samples we have collected
+volatile int16_t aDCVal[dSize];       //array to hold ADC samples
+volatile uint16_t sampleCounter = 0;  //tracks how many samples we have collected
 
 uint8_t countFFT = 0;
 uint16_t indexFFT = 0;
@@ -33,7 +33,7 @@ int32_t prevPressure = 0;
 int32_t prevHumidity = 0;   
 int32_t prevGas = 0;   
 
-long long prevBMEmeasurement = 0;
+long prevBMEmeasurement = 0;
 
 unsigned char packetBuffer[48]; 
 bool tempFlag = false;
@@ -56,9 +56,9 @@ uint8_t TEST_HEAD = 0;
 
 bool newMessage = false;
 
-//char str[254];
+char str[254];
 
-long long prevMessage = 0;
+long prevMessage = 0;
 
 void ADC_Handler();
 void setup(void) {     
@@ -96,43 +96,96 @@ void setup(void) {
     
     setupBME680(&BME680); 
 
-	  clearRegisters();               // disable ADC and clear CTRLA and CTRLB registers
-  	portSetup();                    //setup the ports or pin to make ADC measurement
-	  genericClockSetup(gClk, cDiv);  //setup generic clock and routed it to ADC
-  	aDCSetup();                     //this function set up registers for ADC, input argument sets ADC reference
-  	setUpInterrupt(intPri);         //sets up interrupt for ADC and argument assigns priority
-  	aDCSWTrigger();                 //trigger ADC to start free run mode
+    noInterrupts();
+    clearRegisters();              // disable ADC and clear CTRLA and CTRLB
+    portSetup();                   // setup the ports or pin to make ADC measurement
+    genericClockSetup(gClk, cDiv); // setup generic clock and routed it to ADC
+    aDCSetup();                    // set up registers for ADC, input argument sets ADC reference
+    setUpInterrupt(intPri);        // sets up interrupt for ADC and argument assigns priority
+    aDCSWTrigger();                // trigger ADC to start free run mode
+    interrupts();
 }
 
 uint16_t testCounter = 0;
+
+bool firstLoop = true;
+bool secondLoop = true;
 void loop(void) {   
     uint8_t offset = 1;
     
-    if (millis() - prevBMEmeasurement >= 10000) {
+    if (millis() - prevBMEmeasurement >= 60000 || firstLoop || secondLoop) {
         NVIC_DisableIRQ(ADC_IRQn); // stop ADC_IRQ to read from I2C
-  	    BME680.getSensorData(temp, humidity, pressure, gas);  // Get readings
+        BME680.getSensorData(temp, humidity, pressure, gas);  // Get readings
         NVIC_EnableIRQ(ADC_IRQn);  // enable ADC_IRQ again
-        if (prevTemp - temp >= 100 || prevTemp - temp <= -100) { // difference in 1 degree
-            prevTemp = temp;
-            tempFlag = true;
+        if (!firstLoop) {
+            if (prevTemp - temp >= 100 || prevTemp - temp <= -100) { // difference in 1 degree
+                prevTemp = temp;
+                tempFlag = true;
+            }
+            if (prevPressure - pressure >= 500 || prevPressure - pressure <= -500) { // difference in 10 pa
+                prevPressure = pressure;
+                pressFlag = true;
+            }
+            if (prevHumidity - humidity >= 1000 || prevHumidity - humidity <= -1000) { // difference in 1 %
+                prevHumidity = humidity;
+                humFlag = true;
+            }
+            if (prevGas - gas >= 6000 || prevGas - gas <= -6000) { 
+                prevGas = gas;
+                gasFlag = true;
+            }
+            if (secondLoop) {
+                secondLoop = false;
+            }
+            prevBMEmeasurement = millis();
         }
-        if (prevPressure - pressure >= 500 || prevPressure - pressure <= -500) { // difference in 10 pa
-            prevPressure = pressure;
-            pressFlag = true;
+        else {
+            firstLoop = false;
         }
-        if (prevHumidity - humidity >= 1000 || prevHumidity - humidity <= -1000) { // difference in 1 %
-            prevHumidity = humidity;
-            humFlag = true;
-        }
-        if (prevGas - gas >= 2000 || prevGas - gas <= -2000) {
-            prevGas = gas;
-            gasFlag = true;
-        }
-        prevBMEmeasurement = millis();
     }
 
+    if (sampleCounter == dSize) { 
+        uint32_t amp1 = 0;
+        NVIC_DisableIRQ(ADC_IRQn);
+        removeDCOffset(aDCVal, dSize);
+        for (int i = 0; i < dSize; i++) {
+           amp1 += abs(aDCVal[i]);
+        }
+        amp1 /= dHalfSize;
+        
+        ZeroFFT((int16_t*)aDCVal, dSize);
+        for (int i=0; i < dHalfSize; i++){
+            if (aDCVal[i] > aDCVal[indexFFT]) {
+                indexFFT = i;
+            }
+        }
+
+        amp += amp1;
+        freq += FFT_BIN(indexFFT, sRate, dSize);
+        countFFT++;
+        
+        if (countFFT == 50) {
+            amplitude = amp;
+            frequency = freq;
+            amplitude /= 50;
+            frequency /= 50;
+            //SerialUSB.print(amplitude);
+            //SerialUSB.print("amp  ");
+            //SerialUSB.print(frequency);
+            //SerialUSB.println("Hz"); 
+            freq = 0;
+            amp = 0;
+            countFFT = 0;
+        }
+        
+        indexFFT = 0;
+        sampleCounter = 0;
+        NVIC_EnableIRQ(ADC_IRQn); 
+    }
+    freqFlag = true;
+    ampFlag = true;
     //testFlag = true; 
-    if (countSamples == (dSize-1)) { 
+    /*if (countSamples == (dSize-1)) { 
         NVIC_DisableIRQ(ADC_IRQn); // stop ADC_IRQ to fill values in array
         for (int i=0; i < dSize; i++){
             amp += aDCVal[i];
@@ -170,15 +223,15 @@ void loop(void) {
         indexFFT = 0;
         countSamples = 0;
         NVIC_EnableIRQ(ADC_IRQn);  // enable ADC_IRQ again
-    }
+    }*/
     
-
     if (tempFlag) {
         packetBuffer[0] |= (1 << TEMP_HEAD);
         memmove(packetBuffer+offset, &temp, sizeof(temp));
         tempFlag = false;
         offset += 4;
         newMessage = true;
+        // SerialUSB.println("temp");
     }
     if (humFlag) {
         packetBuffer[0] |= (1 << HUM_HEAD);
@@ -186,6 +239,7 @@ void loop(void) {
         humFlag = false;
         offset += 4;
         newMessage = true;
+        // SerialUSB.println("hum");
     }
     if (pressFlag) {
         packetBuffer[0] |= (1 << PRESS_HEAD);
@@ -193,6 +247,7 @@ void loop(void) {
         pressFlag = false;
         offset += 4;
         newMessage = true;
+        // SerialUSB.println("press");
     }
     if (gasFlag) {
         packetBuffer[0] |= (1 << GAS_HEAD);
@@ -200,13 +255,16 @@ void loop(void) {
         gasFlag = false;
         offset += 4;
         newMessage = true;
+        // SerialUSB.println("gas");
     }
+    
     if (freqFlag) {
         packetBuffer[0] |= (1 << FREQ_HEAD);
         memmove(packetBuffer+offset, &frequency, sizeof(frequency));
         freqFlag = false;
         offset += 4;
         newMessage = true;
+        // SerialUSB.println("freq");
     }
     if (ampFlag) {
         packetBuffer[0] |= (1 << AMP_HEAD);
@@ -214,30 +272,32 @@ void loop(void) {
         ampFlag = false;
         offset += 4;
         newMessage = true;
+        //SerialUSB.println("amp");
     }
     if (testFlag) {
         packetBuffer[0] |= (1 << TEST_HEAD);
         memmove(packetBuffer+offset, &testCounter, sizeof(testCounter));
         testFlag = false;
+        // SerialUSB.println("test");
     }
 
     bool result = false;
     if (millis() - prevMessage >= 10000) {
     //if (newMessage) { 
-        // snprintf((char*)str, 254, "t%d, h%d, p%d, g%d, f %d, a%d, c%d", 
-        //        temp, humidity, pressure, gas, frequency, amplitude, testCounter);
+        //snprintf((char*)str, 254, "t%d, h%d, p%d, g%d, f %d, a%d, c%d", 
+                //temp, humidity, pressure, gas, frequency, amplitude, testCounter);
         
-        // SerialUSB.println(str);
-        //newMessage = false;
+        //SerialUSB.println(str);
+        newMessage = false;
         prevMessage = millis();
         testCounter++;
-        NVIC_DisableIRQ(ADC_IRQn); // stop ADC_IRQ to read from I2C
+        NVIC_DisableIRQ(ADC_IRQn); // stop ADC_IRQ while sending messages
         result = lora.transferPacket(packetBuffer, 100);
         NVIC_EnableIRQ(ADC_IRQn);  // enable ADC_IRQ again
-    // result = lora.transferPacket("t:2700,p:1700,h:5000,g:156,f:440,a:230", 10);
+        memset(packetBuffer, 0, 48);
     }
  
-    /*if(result)
+   /* if(result)
     {
         short length;
         short rssi;
@@ -265,10 +325,10 @@ void loop(void) {
 
 // This ISR is called each time ADC makes a reading
 void ADC_Handler() {
-    if(countSamples < 1023) {
-        aDCVal[countSamples] = REG_ADC_RESULT;
-        countSamples++;
+    if(sampleCounter < dSize) {
+      aDCVal[sampleCounter] = REG_ADC_RESULT;
+      sampleCounter++;
     }
     // Need to reset interrupt
-    ADC->INTFLAG.reg = ADC_INTENSET_RESRDY; 
+    ADC->INTFLAG.reg = ADC_INTENSET_RESRDY;
 }
--- EndNodes/Seeeduino/mic.hh
+++ EndNodes/Seeeduino/mic.hh
@@ -2,78 +2,78 @@
 #define MIC_HH
 void clearRegisters() {
     // clear/disable adc if enabled
-    ADC->CTRLA.reg = 0; 	
+    ADC->CTRLA.reg = 0;   
     while (REG_ADC_STATUS & ADC_STATUS_SYNCBUSY);
-    // clear/disable defaulr arduino ide settings for ctrlb 
-	ADC->CTRLB.reg = 0; 	
+    // clear/disable default arduino ide settings for ctrlb 
+    ADC->CTRLB.reg = 0;   
     while (REG_ADC_STATUS & ADC_STATUS_SYNCBUSY);
 }
 
 // function for configuring ports or pins, note that this will not use the same pin numbering scheme as Arduino, it will use Pxy groups
 // x is the group and y is the number or id 
 void portSetup() {
-  	// Input pin for ADC Arduino A0/PA02
-  	REG_PORT_DIRCLR1 = PORT_PA02;
+    // Input pin for ADC Arduino A0/PA02
+    REG_PORT_DIRCLR1 = PORT_PA02;
 
-  	// Enable multiplexing on PA02_AIN0 and PA03/ADC_VREFA
-  	PORT->Group[0].PINCFG[2].bit.PMUXEN = 1;
-  	PORT->Group[0].PINCFG[3].bit.PMUXEN = 1;
-  	PORT->Group[0].PMUX[1].reg = PORT_PMUX_PMUXE_B | PORT_PMUX_PMUXO_B;
+    // Enable multiplexing on PA02_AIN0 and PA03/ADC_VREFA
+    PORT->Group[0].PINCFG[2].bit.PMUXEN = 1;
+    PORT->Group[0].PINCFG[3].bit.PMUXEN = 1;
+    PORT->Group[0].PMUX[1].reg = PORT_PMUX_PMUXE_B | PORT_PMUX_PMUXO_B;
 }
 
 // this function sets up the generic clock that will be used for the ADC unit
 // by default it uses the 48M system clock, input arguments set divide factor for generic clock and choose which generic clock
-// Note unless you understand how the clock system works use clock 3. clocks 5 and up can brick the microcontroller based on how Arduino configures things
 void genericClockSetup(int clk, int dFactor) {
-  	// Enable the APBC clock for the ADC
-  	REG_PM_APBCMASK |= PM_APBCMASK_ADC;
+    // Enable the APBC clock for the ADC
+    REG_PM_APBCMASK |= PM_APBCMASK_ADC;
   
-  	// This allows you to setup a div factor for the selected clock certain clocks allow certain division factors: Generic clock generators 3 - 8 8 division factor bits - DIV[7:0]
-  	GCLK->GENDIV.reg |= GCLK_GENDIV_ID(clk)| GCLK_GENDIV_DIV(dFactor);
-  	while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);  
-
-  	// configure the generator of the generic clock with 8 MHz clock
-  	// GCLK_GENCTRL_DIVSEL stay unset, it makes divide based on power of two
+    // This allows you to setup a div factor for the selected clock certain clocks allow certain division factors: Generic clock generators 3 - 8 8 division factor bits - DIV[7:0]
+     GCLK->GENDIV.reg |= GCLK_GENDIV_ID(clk)| GCLK_GENDIV_DIV(dFactor);
+     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+     
+    // configure the generator of the generic clock with 8 MHz clock
+    // GCLK_GENCTRL_DIVSEL stay unset, it makes divide based on power of two
     GCLK->GENCTRL.reg |= GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSC8M | GCLK_GENCTRL_ID(clk);
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
   
-  	// enable clock, set gen clock number, and ID to where the clock goes (30 is ADC)
-  	GCLK->CLKCTRL.reg |= GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(clk) | GCLK_CLKCTRL_ID(30);
- 	while (GCLK->STATUS.bit.SYNCBUSY);
+    // enable clock, set gen clock number, and ID to where the clock goes (30 is ADC)
+    GCLK->CLKCTRL.reg |= GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(clk) | GCLK_CLKCTRL_ID(30);
+    while (GCLK->STATUS.bit.SYNCBUSY);
 }
 
-/*
-ADC_CTRLB_PRESCALER_DIV4_Val    0x0u  
-ADC_CTRLB_PRESCALER_DIV8_Val    0x1u   
-ADC_CTRLB_PRESCALER_DIV16_Val   0x2u   
-ADC_CTRLB_PRESCALER_DIV32_Val   0x3u   
-ADC_CTRLB_PRESCALER_DIV64_Val   0x4u   
-ADC_CTRLB_PRESCALER_DIV128_Val  0x5u   
-ADC_CTRLB_PRESCALER_DIV256_Val  0x6u   
-ADC_CTRLB_PRESCALER_DIV512_Val  0x7u   
---> 8 bit ADC measurement takes 5 clock cycles, 10 bit ADC measurement takes 6 clock cycles
---> Using 48MHz system clock with division factor of 1
---> Sample rate =  8 000 000/(16*8*5) = 12500 KSPS
-This function sets up the ADC, including setting resolution and ADC sample rate
-*/
+
+// ADC_CTRLB_PRESCALER_DIV4_Val    0x0u  
+// ADC_CTRLB_PRESCALER_DIV8_Val    0x1u   
+// ADC_CTRLB_PRESCALER_DIV16_Val   0x2u   
+// ADC_CTRLB_PRESCALER_DIV32_Val   0x3u   
+// ADC_CTRLB_PRESCALER_DIV64_Val   0x4u   
+// ADC_CTRLB_PRESCALER_DIV128_Val  0x5u   
+// ADC_CTRLB_PRESCALER_DIV256_Val  0x6u   
+// ADC_CTRLB_PRESCALER_DIV512_Val  0x7u   
+// --> Need of 12.5 kHz freq = 80us
+// --> ADC time = prop. delay(p. 785) + Adj.ADC sample(p. 798) - half cycle(the sample, p. 787)
+// --> Using 8MHz system clock with division factor of 1
+// --> DIV64 -> ADC generic cloak 8MHz/64 = 125 000 -> time = 1/125 000 = 8us (one cycle)
+// --> Prop. delay = (4 + 0(GAIN_1x))/125 000 = 32us
+// --> 80us = 32us + Adj. ADC sample - 4us -> Adj. ADC sample = 52us
+// --> Adj. ADC sample = (samplen+1)*(cycle/2) = (samplen+1)*4us -> samplen = 12
+// This function sets up the ADC, including setting resolution and ADC sample rate
+
 void aDCSetup() {
-  	// set vref for ADC to VCC
-  	REG_ADC_REFCTRL = ADC_REFCTRL_REFSEL_INTVCC1; 
+    // set vref for ADC to VCC
+    REG_ADC_REFCTRL = ADC_REFCTRL_REFSEL_INTVCC1;
 
-  	// average control 8 sample
-  	REG_ADC_AVGCTRL |= ADC_AVGCTRL_SAMPLENUM_8;
-
-  	// Sampling time, no extra sampling half clock-cycles
-    // by the documentation the time we need for sampling is (samplen + 1) * (cycles of mode(in free mode without gain is ~ 8)/2) so actually 5, 
-    // this we use in our formula for selecting clock and divide factor clock/(divide factor*avg samples*5) >= 8 kHz, but no too big
-  	REG_ADC_SAMPCTRL = ADC_SAMPCTRL_SAMPLEN(0);
+    // average control 1 sample
+    // samplen = 12
+    REG_ADC_AVGCTRL |= ADC_AVGCTRL_SAMPLENUM_1;
+    REG_ADC_SAMPCTRL = ADC_SAMPCTRL_SAMPLEN(12); 
   
-  	// Input control and input scan
-  	REG_ADC_INPUTCTRL |= ADC_INPUTCTRL_GAIN_1X | ADC_INPUTCTRL_MUXNEG_GND | ADC_INPUTCTRL_MUXPOS_PIN0;
-  	while (REG_ADC_STATUS & ADC_STATUS_SYNCBUSY);
+    // Input control and input scan
+    REG_ADC_INPUTCTRL |= ADC_INPUTCTRL_GAIN_1X | ADC_INPUTCTRL_MUXNEG_GND | ADC_INPUTCTRL_MUXPOS_PIN0;
+    while (REG_ADC_STATUS & ADC_STATUS_SYNCBUSY);
 
     // set the divide factor, 8 bit resolution and freerun mode
-  	ADC->CTRLB.reg |= ADC_CTRLB_RESSEL_8BIT | ADC_CTRLB_PRESCALER_DIV16 | ADC_CTRLB_FREERUN; 
+    ADC->CTRLB.reg |= ADC_CTRLB_RESSEL_8BIT | ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_FREERUN; 
     while (REG_ADC_STATUS & ADC_STATUS_SYNCBUSY);
 
     // Disable window monitor mode
@@ -91,7 +91,7 @@ void aDCSetup() {
 
 // This function sets up an ADC interrupt that is triggered 
 // when an ADC value is out of range of the window
-// input argument is priority of interrupt (0 is highest priority)
+// input argument is priority of interrupt (0 is highest priority, except RESET, -2 and -1)
 void setUpInterrupt(byte priority) {
   
     // enable ADC ready interrupt
@@ -99,8 +99,8 @@ void setUpInterrupt(byte priority) {
     while (ADC->STATUS.bit.SYNCBUSY);
 
     // enable ADC interrupts
-    NVIC_EnableIRQ(ADC_IRQn); 
     // set priority of the interrupt
+    NVIC_EnableIRQ(ADC_IRQn); 
     NVIC_SetPriority(ADC_IRQn, priority); 
 }
 
@@ -110,11 +110,12 @@ void aDCSWTrigger() {
 }
 
 // This function takes out DC offset of AC signal, it assumes that the offset brings signal to zero volts
-// input arguments: array with measured points and bits of measurement
+// input arguments: array with measured points and size of measurement
 void removeDCOffset(volatile int16_t aDC[], int aSize) {
     int aSteps = 127; // get number of levels in ADC measurement and cut it in half 8-bit = 255 / 2 = 127.5
     for(int i=0; i < aSize; i++) {
         aDC[i] = aDC[i] - aSteps; // take out offset
     }
 }
+
 #endif
